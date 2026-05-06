d8d87b5cf77b482d3bfeabf6eb0bd23b04c363f1
tor.helge.skei <tor.helge.skei@gmail.com>
2026-05-06 12:33:08 +0200

    ...

---
+++ build/build.cpp
@@ -0,0 +1,98 @@
+
+#include "holos.h"
+#include "plugin/holos_plugin.h"
+#include "plugin/holos_entry.h"
+
+//----------------------------------------------------------------------
+//
+//----------------------------------------------------------------------
+
+const clap_plugin_descriptor_t myDescriptor =
+{
+    .clap_version = CLAP_VERSION,
+    .id           = "me/myPlugin",
+    .name         = "my plugin",
+    .vendor       = "me",
+    .url          = "https://my-website.com/my-lugin",
+    .manual_url   = "https://my-website.com/my-lugin/manual.pdf",
+    .support_url  = "https://my-website.com/support",
+    .version      = "0.0.0",
+    .description  = "my plugin",
+    .features     = (const char* [])
+    {
+        CLAP_PLUGIN_FEATURE_AUDIO_EFFECT,
+        nullptr
+    }
+};
+
+//----------------------------------------------------------------------
+//
+//----------------------------------------------------------------------
+
+class myPlugin
+: public HOLOS_Plugin
+{
+    public:
+        myPlugin(const clap_plugin_descriptor_t* ADescriptor, const clap_host_t* AHost);
+        virtual ~myPlugin();
+    private:
+};
+
+//----------------------------------------------------------------------
+//
+//----------------------------------------------------------------------
+
+myPlugin::myPlugin(const clap_plugin_descriptor_t* ADescriptor, const clap_host_t* AHost)
+: HOLOS_Plugin(ADescriptor,AHost)
+{
+}
+
+myPlugin::~myPlugin()
+{
+}
+
+//----------------------------------------------------------------------
+//
+//----------------------------------------------------------------------
+
+// int main()
+// {
+//     return 0;
+// }
+
+//----------------------------------------------------------------------
+//
+//----------------------------------------------------------------------
+
+bool HOLOS_ClapInit(const char *plugin_path)
+{
+    printf("HOLOS_ClapInit\n");
+    return true;
+}
+
+void HOLOS_ClapDeinit()
+{
+    printf("HOLOS_ClapDeinit\n");
+}
+
+//------------------------------
+//
+//------------------------------
+
+const clap_plugin_descriptor_t* HOLOS_GetDescriptor(uint32_t index)
+{
+    printf("HOLOS_GetDescriptor\n");
+    if (index == 0) return &myDescriptor;
+    return nullptr;
+}
+
+HOLOS_Plugin* HOLOS_CreatePlugin(const clap_host_t *host, const char *plugin_id)
+{
+    printf("HOLOS_CreatePlugin\n");
+    if (strcmp(plugin_id,myDescriptor.id) == 0)
+    {
+        HOLOS_Plugin* plugin = new HOLOS_Plugin(&myDescriptor,host);
+        return plugin;
+    }
+    return nullptr;
+}
